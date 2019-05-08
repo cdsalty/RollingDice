7af987f862a9e546d483f633337b9602387c423a
cdsalty <soltiscd@gmail.com>
2019-05-08 14:38:32 -0400

    numbers show on dice, slightly styled

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ class App extends Component {
   render(){
     return (
       <div className = "App">
-        <Die />
+        <Die face='one' />
+        <Die face='one' />
       </div>
     );
   }
 }
 
 export default App;
+
+
+// I created face and then carried it over into the prop defined in die.js
--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
    
     render(){
-        return <i className='fas fa-dice-one'></i>
+        // return <i className='fas fa-dice-one'></i>      // See note 1
+        return <i className = {`DieStyle fas fa-dice-${this.props.face}`}></i>; // created face on App and then rendered as a prop here.
     }
 }
 
 export default Die;
+
+
+/*
+Note 1:
+Originally set as <i className='fas fa-dice-one'></i> but I need a way to pick numbers 1-6, not just 'one'
+set it in JSX, then in the javascript, added our back-tics and extracted this.props.face into it. 
+*/
+
